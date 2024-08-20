1bf5bd84819b5ec4e5a604295a626db44c9305e9
Aram <tommyvtbarros@gmail.com>
2024-08-20 15:01:31 -0400

    Mats + Scripting

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,7 +7,7 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("HOLA");
     }
 
     // Update is called once per frame
